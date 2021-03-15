2b3c1b3db2f5401fa880fe523c2613784fbe3945
JanhviJoshi <33377728+JanhviJoshi@users.noreply.github.com>
2021-03-15 23:03:57 +0530

    Revert "Fully functional application, with password hashing and CSRF protection"

--- models/user.py
+++ models/user.py
@@ -9,23 +9,23 @@ class UserModel(db.Model):
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
     phone_number = db.Column(db.String(10))
-    s_ip = db.Column(db.String(40))
-    s_lat = db.Column(db.String(40))
-    s_long = db.Column(db.String(40))
-    s_timestart = db.Column(db.String(10))
-    s_timeend = db.Column(db.String(10))
+    safe_ip = db.Column(db.String(40))
+    safe_latitude = db.Column(db.String(40))
+    safe_longitude = db.Column(db.String(40))
+    safe_time_start = db.Column(db.String(10))
+    safe_time_end = db.Column(db.String(10))
 
     logs = db.relationship('LogModel', lazy='dynamic')
 
-    def __init__(self, username, password, phone_number, s_ip, s_lat, s_long, s_timestart, s_timeend):
+    def __init__(self, username, password, phone_number, safe_ip, safe_latitude, safe_longitude, safe_time_start, safe_time_end):
         self.username = username
         self.password = password
         self.phone_number = phone_number
-        self.s_ip = s_ip
-        self.s_lat = s_lat
-        self.s_long = s_long
-        self.s_timestart = s_timestart
-        self.s_timeend = s_timeend
+        self.safe_ip = safe_ip
+        self.safe_latitude = safe_latitude
+        self.safe_longitude = safe_longitude
+        self.safe_time_start = safe_time_start
+        self.safe_time_end = safe_time_end
 
     @classmethod
     def find_by_username(cls, username):
